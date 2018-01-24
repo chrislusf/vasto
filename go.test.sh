f605a171783184ed9006a9a87b93c7d1e7294b89
Chris Lu <chris.lu@gmail.com>
2018-01-24 14:48:47 -0800

    skip race detection

--- go.test.sh
+++ go.test.sh
@@ -7,7 +7,7 @@ for d in $(go list ./... \
 	| grep -v vendor  \
 	| grep -v "pb" \
 	| grep -v "doc"); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
