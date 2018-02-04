8430a0a24b2494ce25171e15285788e0da25b760
Chris Lu <chris.lu@gmail.com>
2018-02-04 11:50:47 -0800

    variable rename

--- storage/codec/value.go
+++ storage/codec/value.go
@@ -5,20 +5,23 @@ import (
 	"time"
 )
 
-type EntryValueType byte
+type OpsAndDataType byte
 
 const (
+	// default merge operation is add 
+	// for int64 and float64 : add
+	// for []byte : append
 	// Spell out the entry type and values
-	BYTES   EntryValueType = 0
-	FLOAT64 EntryValueType = 1
+	BYTES   OpsAndDataType = 0
+	FLOAT64 OpsAndDataType = 1
 )
 
 type Entry struct {
-	PartitionHash uint64
-	UpdatedAtNs   uint64
-	TtlSecond     uint32
-	Type          EntryValueType
-	Value         []byte
+	PartitionHash  uint64
+	UpdatedAtNs    uint64
+	TtlSecond      uint32
+	opsAndDataType OpsAndDataType
+	Value          []byte
 }
 
 func (e *Entry) ToBytes() []byte {
@@ -27,7 +30,7 @@ func (e *Entry) ToBytes() []byte {
 	binary.LittleEndian.PutUint64(b, e.PartitionHash)
 	binary.LittleEndian.PutUint64(b[8:], e.UpdatedAtNs)
 	binary.LittleEndian.PutUint32(b[16:], e.TtlSecond)
-	b[20] = byte(e.Type)
+	b[20] = byte(e.opsAndDataType)
 	copy(b[21:], e.Value)
 
 	return b
@@ -40,7 +43,7 @@ func FromBytes(b []byte) *Entry {
 	e.PartitionHash = binary.LittleEndian.Uint64(b[0:8])
 	e.UpdatedAtNs = binary.LittleEndian.Uint64(b[8:16])
 	e.TtlSecond = binary.LittleEndian.Uint32(b[16:20])
-	e.Type = EntryValueType(b[20])
+	e.opsAndDataType = OpsAndDataType(b[20])
 	e.Value = b[21:]
 
 	return e
