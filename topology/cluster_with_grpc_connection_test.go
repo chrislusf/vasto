7a6e883d67e0d83f2c1641620b3c1efeeff44337
Chris Lu <chris.lu@gmail.com>
2018-02-20 00:05:00 -0800

    add one more test for with connection

--- topology/cluster_with_grpc_connection_test.go
+++ topology/cluster_with_grpc_connection_test.go
@@ -14,12 +14,32 @@ func TestClusterWithConnection(t *testing.T) {
 	assert.Equal(t, err != nil, true, "ring 0 with connection to server 2")
 
 	ring3 := createRing(3)
-	err = ring3.WithConnection("failed test", 2, func(node *pb.ClusterNode, conn *grpc.ClientConn) error {
+	err = ring3.WithConnection("test with connection", 2, func(node *pb.ClusterNode, conn *grpc.ClientConn) error {
 
 		assert.Equal(t, node.StoreResource.Address, "localhost:7002", "ring 3 with connection to server 2")
 
 		return nil
 	})
 	assert.Equal(t, err, nil, "ring 0 with connection to server 2")
+
+}
+
+func TestPrimaryShardsWithConnection(t *testing.T) {
+
+	nodes := []*pb.ClusterNode{
+		{
+			StoreResource: &pb.StoreResource{
+				AdminAddress: "localhost:7007",
+			},
+		},
+	}
+
+	err := PrimaryShards(nodes).WithConnection("test shards with conn", 0, func(node *pb.ClusterNode, conn *grpc.ClientConn) error {
+
+		assert.Equal(t, node.StoreResource.AdminAddress, "localhost:7007", "shards with connection to server 0")
+
+		return nil
+	})
+	assert.Equal(t, err, nil, "shards with connection")
 
 }
