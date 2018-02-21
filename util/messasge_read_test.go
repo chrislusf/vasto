217a5dd859b19ddd7e3be4ac9a1706fec555590f
Chris Lu <chris.lu@gmail.com>
2018-02-20 21:41:53 -0800

    add tests on message read

--- util/messasge_read_test.go
+++ util/messasge_read_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 	"math/rand"
 	"bytes"
+	"github.com/magiconair/properties/assert"
+	"io"
+	"encoding/binary"
 )
 
 func TestReadWriteMessage(t *testing.T) {
@@ -23,5 +26,17 @@ func TestReadWriteMessage(t *testing.T) {
 	if bytes.Compare(data, out) != 0 {
 		t.Errorf("unexpected data: %d", bytes.Compare(data, out))
 	}
+
+	_, err = ReadMessage(&buf)
+	assert.Equal(t, err, io.EOF, "end of buffer")
+
+	binary.Write(&buf, binary.LittleEndian, int32(0))
+	out, err = ReadMessage(&buf)
+	assert.Equal(t, err, nil, "zero length message")
+	assert.Equal(t, len(out), 0, "zero length message")
+
+	binary.Write(&buf, binary.LittleEndian, int32(1))
+	out, err = ReadMessage(&buf)
+	assert.Equal(t, err != nil, true, "wrong length message")
 
 }
